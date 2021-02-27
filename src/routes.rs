d249238d7119e2b63e78370f5eb2bf36486b4fb3
tejasag <tejasagarwalbly@gmail.com>
2021-02-27 09:06:23 +0530

    added remove-post route

--- src/routes.rs
+++ src/routes.rs
@@ -4,6 +4,7 @@ use crate::CoolDb;
 use crate::models::Post;
 use std::borrow::Borrow;
 use rocket::Rocket;
+use rocket::response::content;
 
 #[derive(Serialize, Deserialize)]
 struct CreatePostJson {
@@ -24,6 +25,23 @@ fn create_post(input: Json<CreatePostJson>, conn: CoolDb) -> Json<Post> {
 		&input.body,
 		&conn.0
 	))
+}
+
+#[derive(Serialize, Deserialize)]
+struct RemovePostJson {
+	post_id: i32,
+}
+
+#[post("/remove-post", format = "json", data = "<input>")]
+fn remove_post(input: Json<RemovePostJson>, conn: CoolDb) -> Json<&'static str> {
+	use crate::schema::post::dsl::*;
+	use diesel::prelude::*;
+
+	let count = diesel::delete(post.filter(id.eq(input.id))).execute(&conn)?;
+    if count == 0 {
+		return Json("{ 'status': 'false', 'error': 'The specified post was not found.' }")
+	}
+	Json("{ 'status': 'true' }")
 }
 
 pub fn fuel(rocket: Rocket) -> Rocket {
