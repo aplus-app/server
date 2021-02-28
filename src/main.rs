fac55cb977f81ea292d50ba1ffa22f70cd44f409
Will Lane <williamlane923@gmail.com>
2021-02-27 20:40:26 -0800

    added cors (hopefully)

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use rocket_contrib::databases::diesel::PgConnection;
 use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
 use rocket::Response;
-use rocket::http::Header;
+use rocket::http::{Header, Status, ContentType};
 use rocket::fairing::{Kind, Info, Fairing};
 
 #[database("cooldb")]
@@ -50,9 +50,20 @@ impl Fairing for CORS {
   }
 }
 
+#[get("/")]
+fn get_handler<'a>() -> Response<'a> {
+  let mut res = Response::new();
+  res.set_status(Status::new(200, "No Content"));
+  res.adjoin_header(ContentType::Plain);
+  res.adjoin_raw_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.adjoin_raw_header("Access-Control-Allow-Origin", "*");
+  res.adjoin_raw_header("Access-Control-Allow-Credentials", "true");
+  res.adjoin_raw_header("Access-Control-Allow-Headers", "Content-Type");
+  res
+}
+
 fn main() {
   routes::fuel(rocket::ignite())
-    .attach(CORS())
     .attach(CoolDb::fairing())
     .register(catchers![catch_not])
     .mount("/", routes![status])
