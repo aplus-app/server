f7c69915286f99b79606a6fe180bb2afa5953585
Will Lane <williamlane923@gmail.com>
2021-02-27 20:26:12 -0800

    added cors (hopefully)

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ use rocket::request::Request;
 use rocket_contrib::databases::diesel::PgConnection;
 use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
+use rocket::Response;
+use rocket::http::Header;
+use rocket::fairing::{Kind, Info, Fairing};
 
 #[database("cooldb")]
 struct CoolDb(PgConnection);
@@ -29,8 +32,27 @@ fn catch_not(_req: &Request) -> &'static str {
   "Service is not available. (Is the database up?)"
 }
 
+pub struct CORS();
+
+impl Fairing for CORS {
+  fn info(&self) -> Info {
+    Info {
+      name: "Add CORS headers to requests",
+      kind: Kind::Response
+    }
+  }
+
+  fn on_response(&self, request: &Request, response: &mut Response) {
+    response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+    response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+    response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+    response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+  }
+}
+
 fn main() {
   routes::fuel(rocket::ignite())
+    .attach(CORS())
     .attach(CoolDb::fairing())
     .register(catchers![catch_not])
     .mount("/", routes![status])
