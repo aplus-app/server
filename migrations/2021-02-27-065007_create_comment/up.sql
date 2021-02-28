555ba7c8726a78b97e9f52621bc7b8ae18e94379
tejasag <tejasagarwalbly@gmail.com>
2021-02-28 14:19:59 +0530

    added default 0

--- migrations/2021-02-27-065007_create_comment/up.sql
+++ migrations/2021-02-27-065007_create_comment/up.sql
@@ -3,5 +3,5 @@ create table comment
     id        serial primary key not null,
     post_id   varchar            not null,
     body      varchar            not null,
-    user_name varchar            not null
+    user_name varchar            not null default 0
 )
